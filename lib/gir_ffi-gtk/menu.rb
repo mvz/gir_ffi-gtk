6c4d7899bde5095298d5576cdbeb9466af7a67ae
Matijs van Zuijlen <matijs@matijs.net>
2014-03-24 21:02:20 +0100

    Only provide and test Gtk::Menu#popup if needed

--- lib/gir_ffi-gtk/menu.rb
+++ lib/gir_ffi-gtk/menu.rb
@@ -3,11 +3,13 @@ module Gtk
 
   # Overrides for GtkMenu
   class Menu
-    def popup(parent_menu_shell, parent_menu_item, func, data, button, activate_time)
-      popup_for_device(nil,
-                       parent_menu_shell, parent_menu_item, func, data,
-                       nil,
-                       button, activate_time)
+    unless method_defined? :popup
+      def popup(parent_menu_shell, parent_menu_item, func, data, button, activate_time)
+        popup_for_device(nil,
+                         parent_menu_shell, parent_menu_item, func, data,
+                         nil,
+                         button, activate_time)
+      end
     end
   end
 end
--- test/gir_ffi-gtk/menu_test.rb
+++ test/gir_ffi-gtk/menu_test.rb
@@ -1,16 +1,18 @@
 require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
 
 describe Gtk::Menu do
-  describe "#popup" do
-    it "delegates to popup_for_device" do
-      mnu = Gtk::Menu.new
-      mock(mnu).popup_for_device(nil,
-                                  'parent_menu_shell', 'parent_menu_item',
-                                  'func', 'data',
-                                  nil,
-                                  'button', 'activate_time')
-      mnu.popup('parent_menu_shell', 'parent_menu_item', 'func', 'data',
-                'button', 'activate_time')
+  unless GObjectIntrospection::IRepository.default.find_by_name('Gtk', 'Menu').find_method('popup')
+    describe "#popup" do
+      it "delegates to popup_for_device" do
+        mnu = Gtk::Menu.new
+        mock(mnu).popup_for_device(nil,
+                                   'parent_menu_shell', 'parent_menu_item',
+                                   'func', 'data',
+                                   nil,
+                                   'button', 'activate_time')
+        mnu.popup('parent_menu_shell', 'parent_menu_item', 'func', 'data',
+                  'button', 'activate_time')
+      end
     end
   end
 end
