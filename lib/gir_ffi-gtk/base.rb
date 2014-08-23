309d2edd1c93b1b2330d2089ebbccd3ef530a3b6
Matijs van Zuijlen <matijs@matijs.net>
2014-08-23 10:41:28 +0200

    Add idle handler to Gtk.main loop

--- lib/gir_ffi-gtk/base.rb
+++ lib/gir_ffi-gtk/base.rb
@@ -1,26 +1,50 @@
 # Overrides for Gtk module functions
 module GirFFIGtk
-  module BaseOverrides
+  module AutoArgv
     def self.included base
-      base.extend BaseClassOverrides
+      base.extend ClassMethods
       class << base;
         alias_method :init_without_auto_argv, :init
         alias_method :init, :init_with_auto_argv
       end
     end
+
+    module ClassMethods
+      def init_with_auto_argv
+        remaining = init_without_auto_argv([$PROGRAM_NAME, *ARGV]).to_a
+        remaining.shift
+        ARGV.replace remaining
+      end
+    end
   end
 
-  module BaseClassOverrides
-    def init_with_auto_argv
-      remaining = init_without_auto_argv([$PROGRAM_NAME, *ARGV]).to_a
-      remaining.shift
-      ARGV.replace remaining
+  module ThreadEnabler
+    def self.included base
+      base.extend ClassMethods
+      class << base;
+        alias_method :main_without_thread_enabler, :main
+        alias_method :main, :main_with_thread_enabler
+      end
+    end
+
+    module ClassMethods
+      def main_with_thread_enabler
+        case RUBY_ENGINE
+        when 'jruby'
+        when 'rbx'
+        else # 'ruby' most likely
+          GLib::MainLoop::ThreadEnabler.instance.setup_idle_handler
+        end
+        main_without_thread_enabler
+      end
     end
   end
 end
 
 module Gtk
   setup_method "init"
+  setup_method "main"
 
-  include GirFFIGtk::BaseOverrides
+  include GirFFIGtk::AutoArgv
+  include GirFFIGtk::ThreadEnabler
 end
--- test/gir_ffi-gtk/base_test.rb
+++ test/gir_ffi-gtk/base_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
-describe GirFFIGtk::BaseOverrides do
+describe Gtk do
   describe "::init" do
     before do
       module DummyGtk
         def self.init arr
           ["baz", "qux", "zonk"]
         end
-        include GirFFIGtk::BaseOverrides
+
+        include GirFFIGtk::AutoArgv
       end
     end
 
@@ -21,6 +22,28 @@ describe GirFFIGtk::BaseOverrides do
       ARGV.replace ["foo", "bar"]
       DummyGtk.init
       assert_equal ["qux", "zonk"], ARGV.to_a
+    end
+  end
+
+  describe "::main" do
+    it "allows other threads to run" do
+      a = []
+      GLib.timeout_add(GLib::PRIORITY_DEFAULT, 100,
+                       proc { Gtk.main_quit },
+                       nil, nil)
+
+      slow_thread = Thread.new do
+        sleep 0.005
+        a << "During run"
+      end
+
+      a << "Before run"
+      Gtk.main
+      a << "After run"
+
+      slow_thread.join
+
+      a.last.must_equal "After run"
     end
   end
 end
