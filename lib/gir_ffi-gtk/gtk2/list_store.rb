ec3e952312edea05edebf9185d00d858fc3d58dc
Matijs van Zuijlen <matijs@matijs.net>
2015-12-20 11:33:07 +0100

    Correctly set up instance method
    (This is probably not really needed.)

--- lib/gir_ffi-gtk/gtk2/list_store.rb
+++ lib/gir_ffi-gtk/gtk2/list_store.rb
@@ -3,7 +3,7 @@ module Gtk
   # Overrides for GtkListStore
   class ListStore
     setup_method :newv
-    setup_method :set_valuesv
+    setup_instance_method :set_valuesv
 
     alias_method :initialize, :initializev
     alias_method :set, :set_valuesv
