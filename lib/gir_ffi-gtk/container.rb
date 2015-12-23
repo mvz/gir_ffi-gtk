ccf9cc6e8b70bd173f5801e54657d04636f063f7
Matijs van Zuijlen <matijs@matijs.net>
2015-12-23 08:37:08 +0100

    Add override for Container#child_get_property

--- lib/gir_ffi-gtk/container.rb
+++ lib/gir_ffi-gtk/container.rb
@@ -5,5 +5,19 @@ module Gtk
     def add_with_properties(widget, properties)
       add widget
     end
+
+    setup_instance_method :child_get_property
+
+    def child_get_property_with_override(widget, property_name)
+      param_spec = object_class.find_child_property property_name
+      gtype = param_spec.value_type
+      gvalue = GObject::Value.for_gtype gtype
+
+      child_get_property_without_override widget, property_name, gvalue
+      gvalue.get_value
+    end
+
+    alias_method :child_get_property_without_override, :child_get_property
+    alias_method :child_get_property, :child_get_property_with_override
   end
 end
--- test/gir_ffi-gtk/container_test.rb
+++ test/gir_ffi-gtk/container_test.rb
@@ -1,16 +1,24 @@
 require 'test_helper'
 
 describe Gtk::Container do
-  describe '#add_with_properties' do
-    let(:container) { Gtk::Table.new 1, 1, true }
-    let(:widget) { Gtk::Label.new 'foo' }
+  let(:container) { Gtk::Table.new 1, 1, true }
+  let(:widget) { Gtk::Label.new 'foo' }
 
+  describe '#add_with_properties' do
     before do
       container.add_with_properties widget, expand: true, pack_type: :end
     end
 
     it 'adds the widget to the container' do
       container.get_children.to_a.must_equal [widget]
+    end
+  end
+
+  describe '#child_get_property' do
+    it 'fetches the given child property' do
+      container.add widget
+      container.child_set_property(widget, 'left-attach', 1)
+      container.child_get_property(widget, 'left-attach').must_equal 1
     end
   end
 end
