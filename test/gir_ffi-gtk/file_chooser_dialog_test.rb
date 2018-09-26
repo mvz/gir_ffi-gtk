9b2fa60ad3d0f294f425112206215cb84c8fe55e
Matijs van Zuijlen <matijs@matijs.net>
2018-09-26 09:21:05 +0200

    Fetch children from correct area on older Gtk+ versions

--- test/gir_ffi-gtk/file_chooser_dialog_test.rb
+++ test/gir_ffi-gtk/file_chooser_dialog_test.rb
@@ -6,7 +6,11 @@ describe Gtk::FileChooserDialog do
       dialog = Gtk::FileChooserDialog.new 'Foo', nil, :save, [['Bar', :yes]]
       dialog.title.must_equal 'Foo'
       dialog.action.must_equal :save
-      button = dialog.header_bar.children.to_a.last
+      button = if Gtk::MAJOR_VERSION == 3 && Gtk::MINOR_VERSION >= 12
+                 dialog.header_bar.children.to_a.last
+               else
+                 dialog.action_area.children.to_a.last
+               end
       button.label.must_equal 'Bar'
       dialog.response_for_widget(button).must_equal Gtk::ResponseType.to_int(:yes)
     end
