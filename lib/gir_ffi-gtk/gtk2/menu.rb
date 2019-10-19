5b06ad6b8d5e80bc815be457f389b94eab8e8663
Matijs van Zuijlen <matijs@matijs.net>
2019-10-19 15:43:46 +0200

    Fix override for Menu#popup
    - Do not override for Gtk3
    - Test actual Gtk method call instead of delegation
    - Implement override for Gtk2 to match generated method for Gtk3

---
+++ lib/gir_ffi-gtk/gtk2/menu.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+Gtk.load_class :Menu
+module Gtk
+  # Add non-introspected functions to Gtk::Lib
+  module Lib
+    attach_function(:gtk_menu_popup,
+                    [:pointer, :pointer, :pointer,
+                     :pointer, :pointer,
+                     :uint, :uint32 ],
+                    :void)
+  end
+
+  # Overrides for GtkMenu
+  class Menu
+    def popup(parent_menu_shell, parent_menu_item, button, activate_time, &func)
+      _v1 = Gtk::Widget.from(parent_menu_shell)
+      _v2 = Gtk::Widget.from(parent_menu_item)
+      _v3 = Gtk::MenuPositionFunc.from(func)
+      _v4 = GirFFI::ArgHelper.store(_v3)
+      _v5 = button
+      _v6 = activate_time
+      Gtk::Lib.gtk_menu_popup self, _v1, _v2, _v3, _v4, _v5, _v6
+    end
+  end
+end
+
--- test/gir_ffi-gtk/menu_test.rb
+++ test/gir_ffi-gtk/menu_test.rb
@@ -4,19 +4,9 @@ require 'test_helper'
 
 describe Gtk::Menu do
   describe '#popup' do
-    it 'delegates to popup_for_device' do
-      default_method = GObjectIntrospection::IRepository.default.
-        find_by_name('Gtk', 'Menu').
-        find_method('popup')
-      skip 'No overrided needed for Gtk::Menu#popup' if default_method
+    it 'can be called with 4 arguments' do
       mnu = Gtk::Menu.new
-      mock(mnu).popup_for_device(nil,
-                                 'parent_menu_shell', 'parent_menu_item',
-                                 'func', 'data',
-                                 nil,
-                                 'button', 'activate_time')
-      mnu.popup('parent_menu_shell', 'parent_menu_item', 'func', 'data',
-                'button', 'activate_time')
+      mnu.popup(nil, nil, 1, 32)
     end
   end
 end
