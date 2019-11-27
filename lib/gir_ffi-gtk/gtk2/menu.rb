9d9cf37929fcea88ec8bfae26b9c89ce66f507ce
Matijs van Zuijlen <matijs@matijs.net>
2019-11-27 20:32:20 +0100

    Fix Lint/UnderscorePrefixedVariableName offense

--- lib/gir_ffi-gtk/gtk2/menu.rb
+++ lib/gir_ffi-gtk/gtk2/menu.rb
@@ -14,13 +14,13 @@ module Gtk
   # Overrides for GtkMenu
   class Menu
     def popup(parent_menu_shell, parent_menu_item, button, activate_time, &func)
-      _v1 = Gtk::Widget.from(parent_menu_shell)
-      _v2 = Gtk::Widget.from(parent_menu_item)
-      _v3 = Gtk::MenuPositionFunc.from(func)
-      _v4 = GirFFI::ArgHelper.store(_v3)
-      _v5 = button
-      _v6 = activate_time
-      Gtk::Lib.gtk_menu_popup self, _v1, _v2, _v3, _v4, _v5, _v6
+      v1 = Gtk::Widget.from(parent_menu_shell)
+      v2 = Gtk::Widget.from(parent_menu_item)
+      v3 = Gtk::MenuPositionFunc.from(func)
+      v4 = GirFFI::ArgHelper.store(v3)
+      v5 = button
+      v6 = activate_time
+      Gtk::Lib.gtk_menu_popup self, v1, v2, v3, v4, v5, v6
     end
   end
 end
